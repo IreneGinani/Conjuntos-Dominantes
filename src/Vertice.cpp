97a9b322d901ae44257b184f1ddf13a30934a354
João Emmanuel <joaoeizidio@gmail.com>
2017-06-03 10:27:49 -0300

    Classe Vertice feita #erro corrigido

--- src/Vertice.cpp
+++ src/Vertice.cpp
@@ -13,6 +13,6 @@ void Vertice::calcularW_v (double v1, double v2, double v3, double v4) {
 	W_v = (G_v * v1) + (D_v * v2) + (M_v * v3) + (P_v * v4);
 }
 
-int Vertice::getW_v () {
+double Vertice::getW_v () {
 	return W_v;
 }
