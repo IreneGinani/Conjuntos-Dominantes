26dc83557ebe2746249f9f3dff1cd02b9dfae81d
ireneginani <ireneginani@gmail.com>
2017-06-03 14:27:51 -0300

    Atualização da implementação da classe Vértice

--- src/Vertice.cpp
+++ src/Vertice.cpp
@@ -1,4 +1,7 @@
-#include "Vertice.h"
+#include "../include/Vertice.h"
+#include <iostream>
+
+using namespace std;
 
 Vertice::Vertice (int grau_n, int diferenca, double distancia, double mobilidade, double processamento) {
 	grau = grau_n;
@@ -15,4 +18,36 @@ void Vertice::calcularW_v (double v1, double v2, double v3, double v4) {
 
 double Vertice::getW_v () {
 	return W_v;
+}
+
+double Vertice::getD_v () {
+	return D_v;
+}
+
+double Vertice::getP_v () {
+	return P_v;
+}
+
+double Vertice::getM_v () {
+	return W_v;
+}
+
+void Vertice::setW_v (double v) {
+	
+	W_v = v;
+}
+
+void Vertice::setP_v (double v) {
+	
+	P_v = v;
+}
+
+void Vertice::setM_v (double v) {
+	
+	M_v = v;
+}
+
+void Vertice::setD_v (double v) {
+	
+	D_v = v;
 }
